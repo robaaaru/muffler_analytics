326f8265ef2f8f6c6635e640179ceb0cc07eaa54
robaaaru <robertangelotulagan@gmail.com>
2026-03-11 10:54:31 +0800

    added: sqlalchemy sessionmaker for database connections.

--- app/database.py
+++ app/database.py
@@ -7,7 +7,14 @@ load_dotenv()
 
 engine = create_engine(url=f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}")
 
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 
