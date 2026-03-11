fc24be46098ff8707bbf647db3d5c3d78435362b
robaaaru <robertangelotulagan@gmail.com>
2026-03-11 16:07:15 +0800

    added a post method in routers

---
+++ app/routers/transactions.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models import Transaction, Service, Order
+from app.schemas import TransactionCreate, TransactionRead
+
+router = APIRouter()
+
+@router.post("/transactions", response_model=TransactionRead)
+def create_transaction(transaction: TransactionCreate, db: Session = Depends(get_db)):
+    queried_cost = 0
+
+    for a in transaction.orders:
+        costs = db.query(Service).filter(Service.service_id == a.service_id).first()
+        queried_cost += a.quantity * costs.cost 
+    
+    db_transaction = Transaction(created_at=transaction.created_at, custom_price=transaction.custom_price, total_cost = queried_cost)
+
+    db.add(db_transaction)
+    db.flush()
+
+    transaction_id =  db_transaction.transaction_id
+
+    for a in transaction.orders:
+        db_order = Order(transaction_id = transaction_id, service_id = a.service_id, motor_id = a.motor_id, quantity = a.quantity)
+        db.add(db_order)
+  
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+   
+    
+
+    
+
