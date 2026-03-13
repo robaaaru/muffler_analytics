549afa0ec02996b5d8d4821e0a4e339a96f434e3
robaaaru <robertangelotulagan@gmail.com>
2026-03-13 20:13:15 +0800

    finalized motors.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from app.routers import transactions, orders, motors, services
+from app.routers import transactions, motors, services
 
 app = FastAPI()
 
 app.include_router(transactions.router)
-app.include_router(orders.router)
 app.include_router(services.router)
 app.include_router(motors.router)
