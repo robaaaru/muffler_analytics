d44bfbd0c92e3431ac8ff12f26602350a5e5c5fe
robaaaru <robertangelotulagan@gmail.com>
2026-03-13 16:48:38 +0800

    finalized services.py router

--- app/routers/services.py
+++ app/routers/services.py
@@ -29,4 +29,23 @@ def get_service(id: int, db : Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail=f"Service {id} doesn't exist!")
     return db_service
 
+@router.put("/services/{id}", response_model=ServiceRead)
+def update_service(id: int, service: ServiceCreate, db: Session = Depends(get_db)):
+    db_service = db.query(Service).filter(Service.service_id == id).first()
+    if db_service is None:
+        raise HTTPException(status_code=404, detail=f"Service {id} doesn't exist!")
+    db_service.cost = service.cost
+    db_service.service_type = service.service_type
 
+    db.commit()
+    db.refresh(db_service)
+    return db_service
+    
+@router.delete("/services/{id}")
+def delete_service(id: int, db: Session = Depends(get_db)):
+    db_service = db.query(Service).filter(Service.service_id == id).first()
+    if db_service is None:
+        raise HTTPException(status_code=404, detail=f"Service {id} doesn't exist!")
+    db.delete(db_service)
+    db.commit()
+    return {"message":f"Service {id} deleted!"}
