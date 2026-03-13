938ad5673efa929adfa8191dfa0d05b8b3f2f255
robaaaru <robertangelotulagan@gmail.com>
2026-03-13 15:37:20 +0800

    added: get endpoint in router/service

--- app/routers/services.py
+++ app/routers/services.py
@@ -16,3 +16,17 @@ def create_service(service: ServiceCreate, db : Session = Depends(get_db)):
     db.refresh(db_service)
     return db_service
 
+@router.get("/services", response_model=list[ServiceRead])
+def get_services(db : Session = Depends(get_db)):
+    db_services = db.query(Service).all()
+    return db_services
+
+
+@router.get("/services/{id}", response_model=ServiceRead)
+def get_service(id: int, db : Session = Depends(get_db)):
+    db_service = db.query(Service).filter(Service.service_id == id).first()
+    if db_service is None:
+        raise HTTPException(status_code=404, detail=f"Service {id} doesn't exist!")
+    return db_service
+
+
