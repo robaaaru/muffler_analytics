3c07812cabaead885ae2f8fb443316d49f9feccc
robaaaru <robertangelotulagan@gmail.com>
2026-03-14 22:05:17 +0800

    refactor models and schemas: update relationships and field names for orders, services, and transactions

--- app/schemas.py
+++ app/schemas.py
@@ -1,37 +1,10 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional, List
-
-
-class OrderCreate(BaseModel):
-    quantity: int
-    motor_id: int
-    service_id: int
-
-class OrderRead(OrderCreate):
-    class Config:
-        from_attributes = True
-    order_id: int
-    service: ServiceRead
-    motor: MotorRead
-
-class TransactionCreate(BaseModel):
-    custom_price: Optional[float] = None
-    orders: List[OrderCreate]
-    created_at: datetime
-
-class TransactionRead(BaseModel):
-    class Config:
-        from_attributes = True
-    transaction_id: int
-    created_at: datetime
-    custom_price: Optional[float] = None
-    total_cost: float
-    orders: List[OrderRead]
+from typing import List
 
 class ServiceCreate(BaseModel):
     service_type: str
-    cost: float
+    base_price: float
 
 class ServiceRead(ServiceCreate):
     class Config:
@@ -46,3 +19,29 @@ class MotorRead(MotorCreate):
     class Config:
         from_attributes = True
     motor_id: int
+
+class OrderCreate(BaseModel):
+    quantity: int
+    motor_id: int
+    service_id: int
+    order_cost: float   
+
+class OrderRead(OrderCreate):
+    class Config:
+        from_attributes = True
+    order_id: int
+    service: ServiceRead
+    motor: MotorRead
+
+class TransactionCreate(BaseModel):
+    orders: List[OrderCreate]
+    created_at: datetime
+
+class TransactionRead(BaseModel):
+    class Config:
+        from_attributes = True
+    transaction_id: int
+    created_at: datetime
+    total_cost: float
+    orders: List[OrderRead]
+
